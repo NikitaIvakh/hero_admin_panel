8a17608e7435105767db795ed3bca504e3783858
Nikita Ivakh <nikita.ivakh7@gmail.com>
2024-08-22 15:47:42 +0300

    add CSSTransition

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
-import React from 'react'
+import React, { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import App from './components/app/App'
@@ -8,9 +8,9 @@ import './styles/index.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-	<React.StrictMode>
+	<StrictMode>
 		<Provider store={store}>
 			<App />
 		</Provider>
-	</React.StrictMode>
+	</StrictMode>
 )
